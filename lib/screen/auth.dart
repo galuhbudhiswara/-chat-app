b5625d0cb7d847cdf7be7dde0faee24cd3918142
galuhbudhiswara <galuhbudhiswara280705@gmail.com>
2023-06-27 22:22:48 +0700

    storing username

--- lib/screen/auth.dart
+++ lib/screen/auth.dart
@@ -22,6 +22,7 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredPassword = '';
   File? _selectedImage;
   var _isAuthenticating = false;
+  var _enteredUsername = '';
 
   void _submit() async {
     final isValid = _form.currentState!.validate();
@@ -60,7 +61,7 @@ class _AuthScreenState extends State<AuthScreen> {
         await FirebaseFirestore.instance
             .collection('users')
             .doc(userCredentials.user!.uid).set({
-              'username' : 'to be done...',
+              'username' : _enteredUsername,
               'email' : _enteredEmail,
               'image_url' : imageUrl,
             });
@@ -134,6 +135,21 @@ class _AuthScreenState extends State<AuthScreen> {
                             onSaved: (value) {
                               _enteredEmail = value!;
                             },
+                          ),
+                          if (!_isLogin)
+                          TextFormField(
+                            decoration: const InputDecoration(labelText: 'Username'),
+                            enableSuggestions: false,
+
+                            validator: (value) {
+                              if (value == null || value.isEmpty || value.trim().length < 4) {
+                                return 'Please enter at least 4 characters.';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _enteredUsername = value!;
+                           },
                           ),
                           TextFormField(
                             decoration: const InputDecoration(
