4ca337aecf4ec6792b10e808dd35736bc622a75b
galuhbudhiswara <galuhbudhiswara280705@gmail.com>
2023-06-27 22:15:54 +0700

    sending data to firebase

--- lib/screen/auth.dart
+++ lib/screen/auth.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
@@ -53,9 +54,16 @@ class _AuthScreenState extends State<AuthScreen> {
             .child('user_images')
             .child('${userCredentials.user!.uid}.jpg');
 
-            await storageRef.putFile(_selectedImage!);
-            final imageUrl = await storageRef.getDownloadURL();
-            print(imageUrl);
+        await storageRef.putFile(_selectedImage!);
+        final imageUrl = await storageRef.getDownloadURL();
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredentials.user!.uid).set({
+              'username' : 'to be done...',
+              'email' : _enteredEmail,
+              'image_url' : imageUrl,
+            });
       }
     } on FirebaseAuthException catch (error) {
       if (error.code == 'email-already-in-use') {
@@ -146,28 +154,28 @@ class _AuthScreenState extends State<AuthScreen> {
                             height: 12,
                           ),
                           if (!_isAuthenticating)
-                          ElevatedButton(
-                            onPressed: _submit,
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Theme.of(context)
-                                  .colorScheme
-                                  .primaryContainer,
+                            ElevatedButton(
+                              onPressed: _submit,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer,
+                              ),
+                              child: Text(_isLogin ? 'Login' : 'SignUp'),
                             ),
-                            child: Text(_isLogin ? 'Login' : 'SignUp'),
-                          ),
-                          if (_isAuthenticating) 
-                          const CircularProgressIndicator(),
+                          if (_isAuthenticating)
+                            const CircularProgressIndicator(),
                           if (!_isAuthenticating)
-                          TextButton(
-                            onPressed: () {
-                              setState(() {
-                                _isLogin = !_isLogin;
-                              });
-                            },
-                            child: Text(_isLogin
-                                ? 'Create an account'
-                                : 'I already have an accound'),
-                          ),
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isLogin = !_isLogin;
+                                });
+                              },
+                              child: Text(_isLogin
+                                  ? 'Create an account'
+                                  : 'I already have an accound'),
+                            ),
                         ],
                       ),
                     ),
