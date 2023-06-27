93a02ba9adf9e75628ba534c265e0b0ae7b97351
galuhbudhiswara <galuhbudhiswara280705@gmail.com>
2023-06-27 21:02:51 +0700

    adding a user image picker widget

--- lib/screen/auth.dart
+++ lib/screen/auth.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:chat_app/widgets/user_image_picker.dart';
 
 final _firebase = FirebaseAuth.instance;
 
@@ -76,6 +77,7 @@ class _AuthScreenState extends State<AuthScreen> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
+                          if (!_isLogin) UserImagePicker(),
                           TextFormField(
                             decoration: const InputDecoration(
                               labelText: 'Email Adress',
